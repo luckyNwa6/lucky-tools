731a5c281ae301f352c016b36a620335a0a8b1d9
username <2428284043@qq.com>
2024-05-11 09:17:53 +0800

    feat: 全删，开启2.0版本

--- docs/assets/index-eb7eeb9c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as n}from"./index-fdd5459f.js";const o={setup(){return{}}};function r(c,s,a,p,_,u){return t(),n("div",null,"404页面")}const f=e(o,[["render",r]]);export{f as default};
